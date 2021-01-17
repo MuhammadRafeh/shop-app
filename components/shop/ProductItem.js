5caeac6e7d2da4e8e4fe26b3b4297fd29e700cdf
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-17 11:38:57 +0300

    Styled the Product Item

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import propTypes from 'prop-types';
+
+const ProductItem = props => {
+    return (
+        <View style={styles.item}>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri: props.imageUrl }} style={styles.image} />
+            </View>
+            <View style={styles.details}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.price}>${props.price}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button title='View Details' onPress={props.handleViewDetail} />
+                <Button title='To Cart' onPress={props.handleToCart} />
+            </View>
+        </View>
+    );
+}
+
+ProductItem.propTypes = {
+    title: propTypes.string,
+    price: propTypes.any,
+    imageUrl: propTypes.string,
+    handleViewDetail: propTypes.func,
+    handleToCart: propTypes.func
+}
+
+export default ProductItem;
+
+const styles = StyleSheet.create({
+    item: {
+        margin: 20,
+        height: 300,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        borderRadius: 10,
+        backgroundColor: '#fff'
+    },
+    imageContainer: {
+        width: '100%',
+        height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    title: {
+        fontSize: 18,
+        color: 'black'
+    },
+    price: {
+        fontSize: 14,
+        color: '#888'
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        height: '25%',
+        alignItems: 'center',
+        paddingHorizontal: 20
+    }
+});
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,18 @@
 //First screen when we load our app
 import React from 'react';
 import { FlatList, Text } from 'react-native';
+import ProductItem from '../../components/shop/ProductItem';
 import PRODUCTS from '../../data/dummy-data';
 
 const ProductsOverviewScreen = props => {
-    return <FlatList data={PRODUCTS} renderItem={({item}) => <Text>{item.title}</Text>}/>;
+    return <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => (
+            <ProductItem
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl} />)
+        } />
 }
 
 export default ProductsOverviewScreen;

22e777896fefd8707b8545ab907058327d5cd997
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-02-10 09:20:54 +0300

    Intalling redux thunk

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default store
